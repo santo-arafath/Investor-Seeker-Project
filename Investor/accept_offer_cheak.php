bc0e5e6d1d2c5dc33d58cbd8f78740902cbed1b8
santo-arafath <arafath.hossen.santo97193@gmail.com>
2023-08-28 14:58:04 +0600

    Changes  committed: 	modified:   Investor/accept_offer_cheak.php     like added

--- Investor/accept_offer_cheak.php
+++ Investor/accept_offer_cheak.php
@@ -34,6 +34,74 @@ if(isset($_REQUEST['accept-offer']))
                                }
   
                } 
+
+
+
+if(isset($_REQUEST['Like']))
+{
+    // echo "accept";
+
+    
+
+   require "../Admin/includes/configure.php";
+   if(!$connnection)
+   {
+    echo "santo";
+    die("not connected".mysqli_error());
+       
+   }
+   else{
+    $_message="Accepted";
+
+    
+    if(isset($_REQUEST['id'])){
+      $rcv_id=$_REQUEST['id'];
+      $query = "SELECT * FROM `card_information_all` WHERE `id` = '$rcv_id'";
+
+
+
+  $result=mysqli_query($connnection,$query);
+
+
+
+  $count=mysqli_num_rows($result);
+
+  if($count>0){
+
+     
+             
+             $row=mysqli_fetch_assoc($result);
+            
+                  $id=$row['id'];
+
+                  $like=$row['like-count'];
+
+                  $new_value=$like+1;
+                 
+
+
+
+      //  -----------------------
+
+
+    
+
+    $query="UPDATE `card_information_all` SET `like-count`  = '$new_value' WHERE `card_information_all`.`id` = $rcv";
+    $resultt=mysqli_query($connnection,$query);
+    if($resultt){
+       
+       header("location:investor_card_details_show_page.php?id=$rcv");
+    echo "accepted4";
+                           }else{
+                     echo "not Updated";
+                                 }
+      
+                               }
+  
+               } 
+
+            }
  
+         }
 
 ?>
